68a2a54b99e2badee439ec829531ea46e13447de
Sougata Maity <maitysougata724@gmail.com>
2025-03-17 14:39:57 +0530

    updated

--- dicom_handler/management/commands/runapscheduler.py
+++ dicom_handler/management/commands/runapscheduler.py
@@ -2,11 +2,19 @@ import logging
 from django.conf import settings
 from apscheduler.schedulers.background import BackgroundScheduler  # Changed to BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
+from apscheduler.triggers.interval import IntervalTrigger
 from django.core.management.base import BaseCommand
 from django_apscheduler.jobstores import DjangoJobStore
 from django_apscheduler.models import DjangoJobExecution
 from django_apscheduler import util
 from dicom_handler.dicomutils.dicomseriesprocessing import read_dicom_metadata
+from django.core.management import call_command
+from api_client.api_utils import (
+    scan_dicom_folder,
+    poll_pending_transfers,
+    notify_completed_transfers,
+    cleanup_old_transfers
+)
 
 logger = logging.getLogger(__name__)
 
@@ -61,6 +69,44 @@ def send_rtstruct():
   except Exception as e:
     print(e)
 
+
+# deidentify and reidentify dicom data
+@util.close_old_connections
+def deidentify():
+   try:
+      # call_command('deidentify_dicom', '/home/sougata/Documents/draw-client-with-ddis/draw-client/data')
+      deidentified_dir = DicomPathConfig.objects.values("deidentificationfolderpath").first()["deidentificationfolderpath"]
+      call_command('deidentify_dicom', f"{deidentified_dir}")
+      print("Successfully DICOM data deidentified")
+
+   except Exception as e:
+      print(e)
+
+
+import uuid
+@util.close_old_connections
+def reidentify():
+   try:
+      source_dir = os.path.join(settings.BASE_DIR, "rtstruct")
+      output_dir =  DicomPathConfig.objects.values("finalrtstructfolderpath").first()["finalrtstructfolderpath"]
+      for i in get_all_files(output_dir):
+        if i.endswith(".dcm"):
+          ds = pydicom.dcmread(i)
+          sop = ds.SOPInstanceUID
+          shutil.copy2(
+            i,
+            os.path.join(source_dir, f"{sop}-{os.path.basename(i)}")  #os.path.basename(i)
+          )
+      target_dir = DicomPathConfig.objects.values("finalrtstructfolderpath").first()["finalrtstructfolderpath"]
+      target_dir = os.path.join(target_dir, "output", "results")
+
+      call_command('reidentify_rtstructure', f"{source_dir}", f"{target_dir}")
+      print("Reidentify done")
+
+   except Exception as e:
+      print(e)
+
+
 # The `close_old_connections` decorator ensures that database connections, that have become
 # unusable or are obsolete, are closed before and after your job has run. You should use it
 # to wrap any jobs that you schedule that access the Django database in any way. 
@@ -85,11 +131,11 @@ class Command(BaseCommand):
         scheduler = BackgroundScheduler(timezone=settings.TIME_ZONE)  # Use BackgroundScheduler
         scheduler.add_jobstore(DjangoJobStore(), "default")
 
-        # Add your jobs
+        # dicom handler app
         scheduler.add_job(
             draw_dicom,
             trigger=CronTrigger(minute="*/15"),  # Every 15 minutes
-            id="draw_dicom",
+            id="Dicom Series Preparation",
             max_instances=1,
             replace_existing=True,
         )
@@ -98,11 +144,64 @@ class Command(BaseCommand):
         scheduler.add_job(
             send_rtstruct,
             trigger=CronTrigger(minute="*/30"),  # Every 30 minutes
-            id="send_rtstruct",
+            id="Send Rtstruct to Data Store",
             max_instances=1,
             replace_existing=True,
         )
         logger.info("Send rtstruct job added.")
+
+        # deidentify app
+        scheduler.add_job(
+            deidentify,
+            trigger=CronTrigger(minute="*/02"),  
+            id="Deidentify Dicom",
+            max_instances=1,
+            replace_existing=True,
+        )
+        logger.info("deidentify job added.")
+
+        scheduler.add_job(
+            reidentify,
+            trigger=CronTrigger(minute="*/02"),  
+            id="Reidentify Dicom Rtstruct",
+            max_instances=1,
+            replace_existing=True,
+        )
+
+        logger.info("reidentify job added.")
+
+        # api client app
+        scheduler.add_job(
+            scan_dicom_folder,
+            trigger=IntervalTrigger(minutes=1),
+            id='Send Dicom data to remote server',
+            max_instances=1,
+            replace_existing=True
+        )
+        
+        scheduler.add_job(
+            poll_pending_transfers,
+            trigger=IntervalTrigger(minutes=10),
+            id='Check Dicom status on remote server',
+            max_instances=1,
+            replace_existing=True
+        )
+        
+        scheduler.add_job(
+            notify_completed_transfers,
+            trigger=IntervalTrigger(minutes=5),
+            id='Notify remote server after completed transfers',
+            max_instances=1,
+            replace_existing=True
+        )
+        
+        scheduler.add_job(
+            cleanup_old_transfers,
+            trigger=CronTrigger(hour=0),  # Run at midnight
+            id='Cleanup old transfers',
+            max_instances=1,
+            replace_existing=True
+        )    
 
         scheduler.add_job(
             delete_old_job_executions,
