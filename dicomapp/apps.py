293ecae06997fc0be4793620d3fbcbbf54076ac1
Santam Chakraborty <drsantam@gmail.com>
2025-04-12 00:06:55 +0530

    Cleaned git tree

--- dicomapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DicomappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dicomapp'
--- docs/source/how_to_manual_segmentation.rst
+++
@@ -1,5 +0,0 @@
-How to initiate a manual segmentation
-=======================================
-
-
-This document describes how to initiate a manual segmentation. 
